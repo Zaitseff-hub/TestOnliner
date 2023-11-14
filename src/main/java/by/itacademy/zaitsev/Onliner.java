af0e9a36885ece642616489fbdb343d1f543a042
Zaitsev <az.pomocnik@gmail.com>
2023-11-14 18:23:53 +0100

    Initial commit

---
+++ src/main/java/by/itacademy/zaitsev/Onliner.java
@@ -0,0 +1,11 @@
+package by.itacademy.zaitsev;
+
+public class Onliner {
+
+    public static String enterButtonLocator = "//*[@id=\"userbar\"]/div[1]/div/div/div[1]";
+    public static String titleAuthFormLocator = "//*[@id=\"auth-container\"]/div/div[2]/div/div[1]";
+    public static String buttonEnter = "//*[@id=\"auth-container\"]/div/div[2]/div/form/div[3]/button";
+    public static String errorMassageLogin = "//*[@id=\"auth-container\"]/div/div[2]/div/form/div[1]/div/div[2]/div/div/div[2]/div";
+    public static String errorMassagePassword = "//*[@id=\"auth-container\"]/div/div[2]/div/form/div[2]/div/div/div[2]/div";
+
+}
